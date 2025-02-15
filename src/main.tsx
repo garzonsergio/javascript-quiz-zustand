e01bae6bd804312e2d5fd32dfb86b78a3dc3f7f4
Camilo Garzón <scgarzonp@gmail.com>
2025-02-15 15:13:08 -0500

    feature: There was implemented Radix styles library and also the title was created

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import "@radix-ui/themes/styles.css";
+import { Theme, ThemePanel } from '@radix-ui/themes';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme >
+      <App />
+      <ThemePanel/>
+    </Theme>
   </StrictMode>,
 )
